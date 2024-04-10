06d6e1afd9420bd524618a908ea74dafab831b04
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2024-04-10 15:58:46 +0800

    Create index.js

---
+++ Lab3/index.js
@@ -0,0 +1,19 @@
+var dataURL='https://api.unsplash.com/photos?client_id=812193ef71ca946e361ed541979a0cfd91e9419a19235fd05f51ea14233f020a&per_page=30';
+function getimg(){
+    var xhr=new XMLHttpRequest();
+    xhr.open('GET',dataURL,true);
+    xhr.send();
+    xhr.onload=function(){
+        var data=JSON.parse(this.responseText);
+        add_new_img(data);
+    }
+}
+function add_new_img(dataset){
+    var gal=document.getElementById("gallery");
+    dataset.array.forEach(function(item){
+        console.log(item);
+        var img=document.createElement("img");
+        img.setAttribute("src",item.urls.small);
+        gal.appendChild(img);
+    });
+}
