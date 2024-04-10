e090e38caaefaeb2b7a82d858665f75d0d9a6ab1
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2024-04-10 17:22:59 +0800

    Update index.js

--- Lab3/index.js
+++ Lab3/index.js
@@ -1,22 +1,44 @@
-var dataUrl = 'https://api.unsplash.com/photos?client_id=812193ef71ca946e361ed541979a0cfd91e9419a19235fd05f51ea14233f020a&per_page=30';
+var imglist_Url = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=ca370d51a054836007519a00ff4ce59e&per_page=10&format=json&nojsoncallback=1';
+
+var imgSizes_Url = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=ca370d51a054836007519a00ff4ce59e&format=json&nojsoncallback=1';
 
 function getimg() {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', dataUrl, true);
+    xhr.open('GET', imglist_Url, true);
     xhr.send();
-    xhr.onload=function() {
-        if (xhr.status===200) {
-            var data=JSON.parse(this.responseText);
-            add_new_img(data);
-        }
-    };
+    xhr.onload = function() {
+        var data = JSON.parse(this.responseText);
+        var photos = data.photos.photo;
+        add_new_img(photos);
+    }
 }
 
-function add_new_img(dataset){
-    var gal = document.getElementById("gallery");
-    dataset.forEach(function(item) {
-        var img = document.createElement("img");
-        img.setAttribute("src", item.urls.small);
-        gal.appendChild(img);
-    });
+function add_new_img(photos) {
+    var gallery = document.getElementById("gallery");
+    gallery.innerHTML = ""; 
+    
+    for (var i = 0; i < photos.length; i++) {
+        var photo = photos[i];
+        var photoId = photo.id;
+        var photoSizesUrl = imgSizes_Url + '&photo_id=' + photoId;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', photoSizesUrl, true);
+        xhr.send();
+        xhr.onload = function() {
+            var sizesData = JSON.parse(this.responseText);
+
+           
+            var mediumPhotoUrl;
+            sizesData.sizes.size.forEach(function(size) {
+                if (size.label === 'Medium') {
+                    mediumPhotoUrl = size.source;
+                }
+            });
+
+            var img = document.createElement("img");
+            img.setAttribute("src", mediumPhotoUrl);
+            gallery.appendChild(img);
+        }
+    }
 }
