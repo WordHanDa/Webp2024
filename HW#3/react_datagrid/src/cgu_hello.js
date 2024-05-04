78e93475c5c6f4369b31f229e30f0fb5c90f1bfc
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2024-05-05 01:12:35 +0800

    q

---
+++ HW#3/react_datagrid/src/cgu_hello.js
@@ -0,0 +1,7 @@
+import './App.css';
+const styleArgument = { fontSize: '100px', color: 'red' };
+const HelloCGU=()=>{ 
+  return <h1 style = { styleArgument } > hello CGU!! </h1>; 
+}
+
+export default HelloCGU;
