5facd66aaeb39fbcb687edfaca5961a3ca5b988b
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2024-05-08 16:40:55 +0800

    q

--- Lab5/react_game/src/index.js
+++ Lab5/react_game/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Game from './reactgame';
+import './OX.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>
 );
 
